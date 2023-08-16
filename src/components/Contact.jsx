f8f78e2ab95d92dd6a4274a69fd7772124dce8c9
mai1209 <mairacoria1209@gmail.com>
2023-08-16 11:52:24 -0300

    arreglos form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,15 +4,16 @@ import Style from "../style/Contact.module.css";
 export default function Contact() {
   return (
     <div className={Style.container} id='Contact'>
+        <input type="hidden" name="form-name" value="contact" />
       <form className={Style.containerInfo} name="contact" method="post" data-netlify="true" >
-        
+    
         <h2 className={Style.input}>Send Me A Message</h2>
-        <input className={Style.input} type="text" placeholder="First Name" action="/contact" />
-        <input className={Style.input} type="email" placeholder="Email Adress" />
-        <input className={Style.input} type="text" placeholder="Suject" />
-        <textarea className={Style.input} type="text"  placeholder="Message" />
+        <input className={Style.input} name="name" type="text" placeholder="First Name" action="/contact" />
+        <input className={Style.input} name="email"  type="email" placeholder="Email Adress" />
+        <input className={Style.input} name="text" type="text" placeholder="Suject" />
+        <textarea className={Style.input} name="message" type="text"  placeholder="Message" />
         <button type="submit" className={Style.input}>Send Message</button>
-        <input type="hidden" name="form-name" value='contact'/>
+       
       </form>
     </div>
   );
