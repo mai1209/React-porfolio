c501e52d7f1aa8bb5f7cbb3c98e57d0c2a89e142
mai1209 <mairacoria1209@gmail.com>
2023-08-16 11:44:57 -0300

    envio formlario

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,13 +4,15 @@ import Style from "../style/Contact.module.css";
 export default function Contact() {
   return (
     <div className={Style.container} id='Contact'>
-      <form className={Style.containerInfo} netlify>
+      <form className={Style.containerInfo} name="contact" method="post" data-netlify="true" >
+        
         <h2 className={Style.input}>Send Me A Message</h2>
-        <input className={Style.input} type="text" placeholder="First Name" />
+        <input className={Style.input} type="text" placeholder="First Name" action="/contact" />
         <input className={Style.input} type="email" placeholder="Email Adress" />
         <input className={Style.input} type="text" placeholder="Suject" />
         <textarea className={Style.input} type="text"  placeholder="Message" />
         <button type="submit" className={Style.input}>Send Message</button>
+        <input type="hidden" name="form-name" value='contact'/>
       </form>
     </div>
   );
