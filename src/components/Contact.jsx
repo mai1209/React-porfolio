2c038e5a803351c234f5171f4b73bb970ef7276d
mai1209 <mairacoria1209@gmail.com>
2023-08-19 12:26:01 -0300

    arreglo formulario

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,70 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import Style from "../style/Contact.module.css";
 
 export default function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_d8y9lgc",
+        "template_2x8eenk",
+        form.current,
+        "ZPKtw0yoVeEKKWRqR"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("mensaje enviado con exito");
+          window.location.reload();
+          
+
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  
   return (
-    <div className={Style.container} id='Contact'>
-       
-      <form className={Style.containerInfo} name="contact" method="POST" data-netlify="true" >
-        <input  type="hidden" name="form-name" value="contact" />
+    <div className={Style.container} id="Contact">
+      <form className={Style.containerInfo} ref={form} onSubmit={sendEmail}>
+        <input type="hidden" name="form-name" value="contact" />
         <h2 className={Style.input}>Send Me A Message</h2>
-        <input className={Style.input} name="name" type="text" placeholder="First Name" action="/contact" />
-        <input className={Style.input} name="email"  type="email" placeholder="Email Adress" />
-        <input className={Style.input} name="text" type="text" placeholder="Suject" />
-        <textarea className={Style.input} name="message" type="text"  placeholder="Message" />
-        <button type="submit" className={Style.input}>Send Message</button>
-        
+        <input
+          className={Style.input}
+          name="user_name"
+          type="text"
+          placeholder="First Name"
+        />
+        <input
+          className={Style.input}
+          name="user_email"
+          type="email"
+          placeholder="Email Adress"
+        />
+        <input
+          className={Style.input}
+          name="user_subject"
+          type="text"
+          placeholder="Suject"
+        />
+        <textarea
+          className={Style.input}
+          name="message"
+          type="text"
+          placeholder="Message"
+        />
+        <button
+          type="submit"
+          value="Send"
+          className={Style.input}
+        >
+          Send Message
+        </button>
       </form>
     </div>
   );
