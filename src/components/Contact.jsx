1d49af3022b20478ad9772b55ad23215882d501a
mai1209 <mairacoria1209@gmail.com>
2023-09-11 16:35:39 -0300

    form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,37 +1,13 @@
-import React, { useRef } from "react";
-import emailjs from "@emailjs/browser";
+import React  from "react";
+//import emailjs from "@emailjs/browser";
 import Style from "../style/Contact.module.css";
 
 export default function Contact() {
-  const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs
-      .sendForm(
-        "service_d8y9lgc",
-        "template_2x8eenk",
-        form.current,
-        "ZPKtw0yoVeEKKWRqR"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          alert("mensaje enviado con exito");
-          window.location.reload();
-          
-
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
   
   return (
     <div className={Style.container} id="Contact">
-      <form className={Style.containerInfo} ref={form} onSubmit={sendEmail}>
+      <form className={Style.containerInfo} >
         <input type="hidden" name="form-name" value="contact" />
         <h2 className={Style.input}>Send Me A Message</h2>
         <input
